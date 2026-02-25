2b3bd1931d58c6eb983e111a40086369a8be4283
anki <an.kiselev.2008@gmail.com>
2026-02-26 01:06:07 +0300

    WORK WORK fixed not specifying segments

--- test.asm
+++ test.asm
@@ -5,17 +5,29 @@ org 100h
 
 Start:
         xor di, di
+
+        Next:
+
+        std
+
         mov ax, 1111h
         mov bx, 2222h
         mov cx, 3333h
         mov dx, 4444h
 
-        L2:
-        mov cx, -1
-        ; @@Next:
-        ; loop @@Next
         inc di
-        jmp L2
+
+        cld
+
+        in al, 60h
+
+        ; scan code of end on kb
+        cmp al, 79d
+        je Exit
+
+        jmp Next
+
+Exit:
 
         mov ax, 4c00h
         int 21h
